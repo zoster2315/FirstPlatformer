499d5f815e5c9f73929283687f124158c1a98e10
zoster2315 <zoster2315@gmail.com>
2021-01-26 21:00:08 +0400

    Первый комит

---
+++ Assets/JumpScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+    public float JumpForce;
+    private Rigidbody2D rb2d;
+    private float distToGround;
+    private CapsuleCollider2D collid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        collid = GetComponent<CapsuleCollider2D>();
+        distToGround = collid.bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+    }
+
+    private void Jump()
+    {
+        var moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(0, 1);
+
+        if (IsGrounded() && moveVertical > 0)
+            rb2d.AddForce(movement * JumpForce);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector2.up, distToGround + 0.1f);
+    }
+}
---
+++ Assets/MovingScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingScript : MonoBehaviour
+{
+    public float speed;
+    public float maxSpeed;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        var velocity = rb2d.velocity;
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(0, 0);
+        if (moveHorizontal > 0)
+        {
+            movement.x = 1;
+            if (velocity.x < 0)
+                movement.x = 2;
+        }
+        else if (moveHorizontal < 0)
+        {
+            movement.x = -1;
+            if (velocity.x > 0)
+                movement.x = -2;
+        }
+
+        if (velocity.x < maxSpeed && velocity.x > -maxSpeed)
+            rb2d.AddForce(movement * speed);
+
+        if (moveHorizontal == 0f)
+        {
+            if (velocity.x > 0)
+                movement.x = -1;
+            else
+                movement.x = 1;
+            rb2d.AddForce(movement * speed);
+        }
+    }
+}
